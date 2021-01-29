2ffa372f75ea9f1c022f61e2e90783767dd25dbe
skichrome <toni.campeol@outlook.fr>
2021-01-29 19:49:40 +0100

    Ajout des librairies manquantes au projet Fix du code restant de main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoLowPower.h>
 
 /*
  * Auto indent in VSCode on Linux: ctrl + shift + i
@@ -9,22 +10,18 @@
 #include "utils/DebugLed.h"
 
 #include "sensors/LuxSensor.h"
-#include "transmitter/WirelessManager.h"
 
 #define CE_PIN 7
 #define CSN_PIN 8
 
 Runnable *Runnable::headRunnable = NULL;
 
-DebugLed led = DebugLed(LED_BUILTIN_TX);
+DebugLed led = DebugLed(LED_BUILTIN);
 
 LuxSensor luxSensor = LuxSensor(led);
 
-WirelessManager rfManager = WirelessManager(CE_PIN, CSN_PIN);
-
 void setup()
 {
-    pinMode(LED_BUILTIN_RX, INPUT); // shut down rx led
     Serial.begin(115200);
     Runnable::setupAll();
 }
